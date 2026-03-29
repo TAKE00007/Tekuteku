de235ceccb4ce80ee489de267a6669e3f5bddecc
TAKE00007 <xshun1590@icloud.com>
2026-03-29 10:25:18 +0900

    LocationManagerに関するServiceを追加

---
+++ Tekuteku/Application/LocationServiceClient.swift
@@ -0,0 +1,102 @@
+import Foundation
+import CoreLocation
+import ComposableArchitecture
+
+struct LocationServiceClient {
+    var requestWhenInUserAuthorization:@MainActor @Sendable  () -> Void
+    var locationUpdates: @MainActor @Sendable () -> AsyncStream<CLLocationCoordinate2D>
+}
+
+extension LocationServiceClient: DependencyKey {
+    static let liveValue: Self = {
+        Self(
+            requestWhenInUserAuthorization:  {
+                let holder = CLLocationManagerHolder.shared
+                let manager = holder.manager
+                manager.requestWhenInUseAuthorization()
+            },
+            locationUpdates: {
+                AsyncStream { continuation in
+                    let holder = CLLocationManagerHolder.shared
+                    let manager = holder.manager
+                    let delegate = LocationManagerDelegateProxy(
+                        didUpdateLocation: { coordinate in
+                            continuation.yield(coordinate)
+                        },
+                        didFinish: {
+                            continuation.finish()
+                        }
+                    )
+                    holder.delegate = delegate
+                    manager.delegate = delegate
+                    
+                    switch manager.authorizationStatus {
+                    case .notDetermined:
+                        manager.requestWhenInUseAuthorization()
+                    case .authorizedAlways, .authorizedWhenInUse:
+                        manager.startUpdatingLocation()
+                    case .denied, .restricted:
+                        continuation.finish()
+                    @unknown default:
+                        continuation.finish()
+                    }
+                    
+                    continuation.onTermination = { _ in
+                        manager.stopUpdatingLocation()
+
+                    }
+                }
+            }
+        )
+    }()
+}
+
+extension DependencyValues {
+    var locationServiceClient: LocationServiceClient {
+        get { self[LocationServiceClient.self] }
+        set { self[LocationServiceClient.self] = newValue }
+    }
+}
+
+@MainActor
+private final class CLLocationManagerHolder {
+    static let shared = CLLocationManagerHolder()
+    
+    let manager = CLLocationManager()
+    var delegate: LocationManagerDelegateProxy?
+}
+
+private final class LocationManagerDelegateProxy: NSObject, CLLocationManagerDelegate {
+    private let didUpdateLocation: (CLLocationCoordinate2D) -> Void
+    private let didFinish: () -> Void
+    
+    init(
+        didUpdateLocation: @escaping (CLLocationCoordinate2D) -> Void,
+        didFinish: @escaping () -> Void
+    ) {
+        self.didUpdateLocation = didUpdateLocation
+        self.didFinish = didFinish
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .authorizedAlways, .authorizedWhenInUse:
+            manager.startUpdatingLocation()
+        case .denied, .restricted:
+            didFinish()
+        case .notDetermined:
+            break
+        @unknown default:
+            didFinish()
+        }
+    }
+    
+    func locationManager(_ merger: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let coordinate = locations.last?.coordinate else { return }
+        didUpdateLocation(coordinate)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        didFinish()
+    }
+}
