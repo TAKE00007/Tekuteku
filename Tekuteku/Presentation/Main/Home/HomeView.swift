4092cae37e6907ea47e0bd705bb00b55217cce1e
TAKE00007 <xshun1590@icloud.com>
2026-03-29 10:25:43 +0900

    責務を明確にした

--- Tekuteku/Presentation/Main/Home/HomeView.swift
+++ Tekuteku/Presentation/Main/Home/HomeView.swift
@@ -1,26 +1,15 @@
 import SwiftUI
-import Combine
 import ComposableArchitecture
 import MapKit
-import CoreLocation
 
 struct HomeView: View {
-    let store: StoreOf<HomeFeature>
-    @State var position: MapCameraPosition = .automatic
-    @StateObject private var locationManager = LocationManager()
+    @Bindable var store: StoreOf<HomeFeature>
     @Namespace var mapScope
-
-    var coursePolyline: MKPolyline? {
-        guard let course = store.course, course.route.count >=  2 else {
-            return nil
-        }
-        return course.route.mkPolyline
-    }
     
     var body: some View {
         ZStack {
-            Map(position: $position, scope: mapScope) {
-                if let coursePolyline {
+            Map(position: $store.position, scope: mapScope) {
+                if let coursePolyline = store.course?.route.mkPolyline {
                     MapPolyline(coursePolyline)
                         .stroke(.blue, lineWidth: 5)
                 }
@@ -32,69 +21,73 @@ struct HomeView: View {
                         .mapControlVisibility(.hidden)
                 })
                 .overlay(alignment: .bottomTrailing) {
-                    VStack {
-                        MapCompass(scope: mapScope)
-                            .mapControlVisibility(.visible)
-                        VStack(spacing: 8) {
-                            Button {
-                                print( "" )
-                            } label: {
-                                Image(systemName: "map.fill")
-                                    .foregroundStyle(.black)
-                                    .font(.title2)
-                                    .frame(width: 44, height: 44)
-                            }
-                            
-                            Button {
-                                position = .userLocation(followsHeading: false, fallback: .automatic)
-                            } label: {
-                                Image(systemName: "location.fill")
-                                    .foregroundStyle(.blue)
-                                    .font(.title2)
-                                    .frame(width: 44, height: 44)
-                            }
-                        }
-                        .background(.thickMaterial, in: RoundedRectangle(cornerRadius: 14, style: .continuous))
-                        
-                        Button {
-                            store.send(.tapWalking)
-                        } label: {
-                            Image(systemName: "figure.walk")
-                                .foregroundStyle(.black)
-                                .font(.title2)
-                                .padding(10)
-                                .background(.thickMaterial, in: Circle())
-                        }
-                        
-                    }
-                    .padding( .trailing, 10)
-                    .buttonBorderShape(.circle)
+                    FlotingButtons(
+                        mapScope: mapScope,
+                        updatePosition: { store.send(
+                            .updatePosition(.userLocation(followsHeading: false, fallback: .automatic)))
+                        },
+                        tapWalking: { store.send(.tapWalking) })
                 }
                 .mapScope(mapScope)
                 .onChange(of: store.course?.id) { _, _ in
-                    guard let coursePolyline else { return }
-                    position = .rect(coursePolyline.boundingMapRect)
+                    guard let coursePolyline = store.course?.route.mkPolyline else { return }
+                    store.send(.updatePosition(.rect(coursePolyline.boundingMapRect)))
                 }
         }
         .task {
-            locationManager.requestPermission()
-            locationManager.startUpdatingLocation()
             store.send(.onAppear)
         }
-        .onReceive(locationManager.$currentLocation.compactMap { $0 } ) { location in
-            store.send(.currentLocationUpdated(location))
-            if position == .automatic {
-                position = .userLocation(followsHeading: false, fallback: .automatic)
-            }
-        }
         .sheet(
-            isPresented: Binding(
-                get: { store.isWalkingSheetPresented },
-                set: { store.send(.setWalkingSheet(isPresented: $0)) }
-            )) {
+            isPresented: $store.isWalkingSheetPresented
+        ) {
                 SliderView(store: store.scope(state: \.slider, action: \.slider))
                 .presentationDetents([.fraction(0.25), .medium])
             }
+    }
+}
+
+struct FlotingButtons: View {
+    let mapScope: Namespace.ID
+    let updatePosition: () -> Void
+    let tapWalking: () -> Void
+    var body: some View {
+        VStack {
+            MapCompass(scope: mapScope)
+                .mapControlVisibility(.visible)
+            VStack(spacing: 8) {
+                Button {
+                    print( "" )
+                } label: {
+                    Image(systemName: "map.fill")
+                        .foregroundStyle(.black)
+                        .font(.title2)
+                        .frame(width: 44, height: 44)
+                }
+                
+                Button {
+                    updatePosition()
+                } label: {
+                    Image(systemName: "location.fill")
+                        .foregroundStyle(.blue)
+                        .font(.title2)
+                        .frame(width: 44, height: 44)
+                }
+            }
+            .background(.thickMaterial, in: RoundedRectangle(cornerRadius: 14, style: .continuous))
+            
+            Button {
+                tapWalking()
+            } label: {
+                Image(systemName: "figure.walk")
+                    .foregroundStyle(.black)
+                    .font(.title2)
+                    .padding(10)
+                    .background(.thickMaterial, in: Circle())
+            }
+            
+        }
+        .padding( .trailing, 10)
+        .buttonBorderShape(.circle)
     }
 }
 
