87e544179099180aed9e04e6b9220e98b2d0699f
TAKE00007 <xshun1590@icloud.com>
2026-04-16 21:04:17 +0900

    Coordinateに距離を計算するメソッドを追加

--- Tekuteku/Domain/Entitiy/Coordinate.swift
+++ Tekuteku/Domain/Entitiy/Coordinate.swift
@@ -4,3 +4,25 @@ struct Coordinate: Equatable, Sendable {
     let latitude: Double
     let longitude: Double
 }
+
+extension Coordinate {
+    nonisolated func distance(to other: Coordinate) -> Double {
+        let earthRadius = 6_371_000.0
+
+        let lat1 = latitude * .pi / 180
+        let lon1 = longitude * .pi / 180
+        let lat2 = other.latitude * .pi / 180
+        let lon2 = other.longitude * .pi / 180
+
+        let dLat = lat2 - lat1
+        let dLon = lon2 - lon1
+
+        let a =
+            sin(dLat / 2) * sin(dLat / 2) +
+            cos(lat1) * cos(lat2) *
+            sin(dLon / 2) * sin(dLon / 2)
+
+        let c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        return earthRadius * c
+    }
+}
