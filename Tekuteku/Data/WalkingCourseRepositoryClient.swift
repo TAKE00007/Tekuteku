1fe7f3eb028c874b05725051b7ed1cd4bf53f7be
TAKE00007 <xshun1590@icloud.com>
2026-04-21 20:54:47 +0900

    withTaskGroupを使った書き方に書き換えた

--- Tekuteku/Data/WalkingCourseRepositoryClient.swift
+++ Tekuteku/Data/WalkingCourseRepositoryClient.swift
@@ -53,46 +53,31 @@ enum WalkingRouteBuilder {
         let bearingDegreesList: [Double] = [0.0, 90.0, 180.0, 270.0] // スタートする方向
         let scale: Double = 0.7 // 1km先の地点でも歩くと長くなるので、短めにして補正する
         
-        var walkingCourses: [WalkingCourse] = []
         
         let edgeDistance = (distanceMeters * scale) / 4
         
-        for bearingDegree in bearingDegreesList {
-            let firstPoint = fetchPoint(from: start, distanceMeters: edgeDistance, bearingDegrees: bearingDegree)
-            let secondPoint = fetchPoint(from: firstPoint, distanceMeters: edgeDistance, bearingDegrees: bearingDegree + 90)
-            let thirdPoint = fetchPoint(from: secondPoint, distanceMeters: edgeDistance, bearingDegrees: bearingDegree + 180)
-            
-            do {
-                async let firstRoute = fetchWalkingRoute(from: start, to: firstPoint)
-                async let secondRoute = fetchWalkingRoute(from: firstPoint, to: secondPoint)
-                async let thirdRoute = fetchWalkingRoute(from: secondPoint, to: thirdPoint)
-                async let finalRoute = fetchWalkingRoute(from: thirdPoint, to: start)
-                
-                let routes = try await [firstRoute, secondRoute, thirdRoute, finalRoute]
-                
-                let mergeRoute = mergeRoutes(routes)
-                let totalDistance = routes.map(\.distance).reduce(0, +) / 1_000
-                let expectedMinutes = max(1, Int((totalDistance) / 4) * 60)
-                let stepCount = totalDistance * 1_000 / 0.7 // 1歩0.7m
-                let calories = stepCount * 0.04 // 1歩0.04kcal
-                
-                let walkingCourse = WalkingCourse(
-                    id: UUID(),
-                    route: mergeRoute,
-                    stepCount: Int(stepCount),
-                    distance: totalDistance,
-                    expectedMinutes: expectedMinutes,
-                    calories: Int(calories)
-                )
-                
-                walkingCourses.append(walkingCourse)
-            } catch let error as WalkingCourseError{
-                throw error
-            } catch {
-                throw WalkingCourseError.mapKitError(error.localizedDescription)
+        let walkingCourses = await withTaskGroup(of: WalkingCourse?.self) { group in
+            for bearingDegree in bearingDegreesList {
+                group.addTask {
+                    do {
+                        let walkingCourse = try await makeWalkingCourse(from: start, edgeDistance: edgeDistance, bearingDegree: bearingDegree)
+                        return walkingCourse
+                    } catch {
+                        return nil
+                    }
+                }
             }
+                    
+            var walkingCourses: [WalkingCourse] = []
+            
+            for await walkingCourse in group {
+                if let walkingCourse = walkingCourse {
+                    walkingCourses.append(walkingCourse)
+                }
+            }
+            return walkingCourses
         }
-
+        
         guard !walkingCourses.isEmpty else {
             throw WalkingCourseError.routeNotFound
         }
@@ -102,6 +87,42 @@ enum WalkingRouteBuilder {
         }
         
         return sortedWalkingCourses
+    }
+    
+    static func makeWalkingCourse(from start: CLLocationCoordinate2D, edgeDistance: Double, bearingDegree: Double) async throws -> WalkingCourse {
+        let firstPoint = fetchPoint(from: start, distanceMeters: edgeDistance, bearingDegrees: bearingDegree)
+        let secondPoint = fetchPoint(from: firstPoint, distanceMeters: edgeDistance, bearingDegrees: bearingDegree + 90)
+        let thirdPoint = fetchPoint(from: secondPoint, distanceMeters: edgeDistance, bearingDegrees: bearingDegree + 180)
+
+        async let firstRoute = fetchWalkingRoute(from: start, to: firstPoint)
+        async let secondRoute = fetchWalkingRoute(from: firstPoint, to: secondPoint)
+        async let thirdRoute = fetchWalkingRoute(from: secondPoint, to: thirdPoint)
+        async let finalRoute = fetchWalkingRoute(from: thirdPoint, to: start)
+        
+        do {
+            let routes = try await [firstRoute, secondRoute, thirdRoute, finalRoute]
+            
+            let mergeRoute = mergeRoutes(routes)
+            let totalDistance = routes.map(\.distance).reduce(0, +) / 1_000
+            let expectedMinutes = max(1, Int((totalDistance) / 4) * 60)
+            let stepCount = totalDistance * 1_000 / 0.7 // 1歩0.7m
+            let calories = stepCount * 0.04 // 1歩0.04kcal
+            
+            let walkingCourse = WalkingCourse(
+                id: UUID(),
+                route: mergeRoute,
+                stepCount: Int(stepCount),
+                distance: totalDistance,
+                expectedMinutes: expectedMinutes,
+                calories: Int(calories)
+            )
+            
+            return walkingCourse
+        } catch let error as WalkingCourseError{
+            throw error
+        } catch {
+            throw WalkingCourseError.mapKitError(error.localizedDescription)
+        }
     }
     
     static func mergeRoutes(_ routes: [MKRoute]) -> [Coordinate] {
