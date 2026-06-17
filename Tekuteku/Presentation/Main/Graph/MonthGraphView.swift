b1544949e7072d402ae9a4e7a399b9354d85c618
TAKE00007 <xshun1590@icloud.com>
2026-06-17 22:08:39 +0900

    選択した時に棒が出るようにした

--- Tekuteku/Presentation/Main/Graph/MonthGraphView.swift
+++ Tekuteku/Presentation/Main/Graph/MonthGraphView.swift
@@ -3,10 +3,11 @@ import Charts
 
 struct MonthGraphView: View {
     let sampleData: [DailyRecord] = MockWeeklyHistoryData.twelveWeeks
+    private static let calendar = Calendar.current
     
-    @State private var selectedDate: Date? = nil
+    @State private var rawSelectedDate: Date?
+    @State private var selectedData: DailyRecord?
     @State private var scrollPosition: Date = {
-        let calendar = Calendar.current
         let lastDate = MockWeeklyHistoryData.twelveWeeks.last?.date ?? Date()
         return calendar.date(byAdding: .day, value: -6, to: lastDate) ?? lastDate
     }()
@@ -17,7 +18,7 @@ struct MonthGraphView: View {
             
             VStack(alignment: .leading) {
 
-                if selectedDate != nil {
+                if selectedData == nil {
                     Text("平均")
                         .font(.caption)
                         .foregroundStyle(.gray)
@@ -45,6 +46,20 @@ struct MonthGraphView: View {
             Spacer()
             
         }
+        .onChange(of: rawSelectedDate) { _, newValue in
+            guard let newValue else {
+                selectedData = nil
+                return
+            }
+            
+            let newData = handleSelection(selectedDate: newValue)
+            
+            if let selectedData, selectedData == newData {
+                self.selectedData = nil
+            } else {
+                selectedData = newData
+            }
+        }
     }
     
     private var chartContent: some View {
@@ -57,6 +72,7 @@ struct MonthGraphView: View {
         .chartScrollPosition(x: $scrollPosition)
         .chartScrollableAxes(.horizontal)
         .chartXVisibleDomain(length: 30 * 24 * 60 * 60)
+        .chartXSelection(value: $rawSelectedDate)
         .chartXAxis {
             AxisMarks(values: .stride(by: .month)) { _ in
                 AxisGridLine(stroke: StrokeStyle(lineWidth: 0.5))
@@ -71,6 +87,47 @@ struct MonthGraphView: View {
                 }
             }
         }
+        .chartBackground { chartProxy in
+            selectionBackground(chartProxy: chartProxy)
+        }
+    }
+    
+    private func selectionBackground(chartProxy: ChartProxy) -> some View {
+        GeometryReader { geometry in
+            if let selectedData,
+               let plotFrame = chartProxy.plotFrame,
+               let x = centerX(for: selectedData.date, chartProxy: chartProxy) {
+                let frame = geometry[plotFrame]
+                let lineX = frame.minX + x
+                
+                Rectangle()
+                    .fill(.gray.opacity(0.3))
+                    .frame(width: 2, height: frame.height + 40)
+                    .position(
+                        x: lineX,
+                        y: frame.minY + frame.height / 2 - 20
+                    )
+            }
+        }
+    }
+    
+    private func centerX(for date: Date, chartProxy: ChartProxy) -> CGFloat? {
+        let startOfDay = MonthGraphView.calendar.startOfDay(for: date)
+        
+        guard
+            let nextDay = MonthGraphView.calendar.date(byAdding: .day, value: 1, to: startOfDay),
+            let startX = chartProxy.position(forX: startOfDay),
+            let endX = chartProxy.position(forX: nextDay)
+        else {
+            return nil
+        }
+        
+        return (startX + endX) / 2
+    }
+    
+    private func handleSelection(selectedDate: Date) -> DailyRecord? {
+        let closet = sampleData.min(by: { abs($0.date.timeIntervalSince(selectedDate)) < abs($1.date.timeIntervalSince(selectedDate)) })
+        return sampleData.first { data in data.date == closet?.date }
     }
 }
 
